2b50a3d45065eb28a83a4fe216791d35f8f173dc
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-11-14 16:39:48 +0200

    Update controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,16 @@
+const User = require('../models/User')
+
 const getSignup = (req, res) => {
     res.send('Will be signup page')
 }
-const postSignup = (req, res) => {
+const postSignup = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({ email, password });
+    } catch (error) {
+        res.status(400).send('An error occurs while trying to create a new user')
+    }
     res.send('Here should post')
 }
 
